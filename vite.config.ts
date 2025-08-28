44fe3b541f9717a01a27d51b2526350626066773
danie1-parker <Daniel.Parker@dv.net>
2025-08-28 14:15:12 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,56 +5,61 @@ import { extname, relative, resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import svgLoader from "vite-svg-loader";
-export default defineConfig({
-  plugins: [
-    vue(),
-    dts({ include: ["./src/lib"] }),
-    svgLoader({
-      svgoConfig: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                removeViewBox: false
-              }
-            }
-          },
-          "prefixIds"
-        ]
-      }
-    })
-  ],
-  build: {
-    copyPublicDir: false,
-    rollupOptions: {
-      external: ["vue", "vue-router"],
-      input: Object.fromEntries(
-        glob
-          .sync("./src/lib/**/*.{ts,vue}", {
-            ignore: ["./src/lib/**/*.d.ts"]
-          })
-          .map((file) => [
-            relative("./src/lib", file.slice(0, file.length - extname(file).length)),
-            fileURLToPath(new URL(file, import.meta.url))
-          ])
-      ),
-      output: {
-        assetFileNames: "[name][extname]",
-        entryFileNames: "[name].js",
-        chunkFileNames: `chunks/[name]-[hash].js`
-      }
-    },
-    lib: {
-      entry: resolve(__dirname, "./src/lib/index.ts"),
-      formats: ["es"],
-      cssFileName: "style"
-    }
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+
+export default defineConfig(({ mode }) => {
+  const isDemo = mode === "demo";
+
+  return {
+    plugins: [
+     vue(),
+     dts({ include: ["./src/lib"] }),
+     svgLoader({
+       svgoConfig: {
+         multipass: true,
+         plugins: [
+           {
+             name: "preset-default",
+             params: {
+               overrides: {
+                 removeViewBox: false
+               }
+             }
+           },
+           "prefixIds"
+         ]
+       }
+     })
+    ],
+    build: isDemo
+     ? {}
+     : {
+       copyPublicDir: false,
+       rollupOptions: {
+         external: ["vue", "vue-router"],
+         input: Object.fromEntries(
+           glob
+             .sync("./src/lib/**/*.{ts,vue}", { ignore: ["./src/lib/**/*.d.ts"] })
+             .map((file) => [
+               relative("./src/lib", file.slice(0, file.length - extname(file).length)),
+               fileURLToPath(new URL(file, import.meta.url))
+             ])
+         ),
+         output: {
+           assetFileNames: "[name][extname]",
+           entryFileNames: "[name].js",
+           chunkFileNames: `chunks/[name]-[hash].js`
+         }
+       },
+       lib: {
+         entry: resolve(__dirname, "./src/lib/index.ts"),
+         formats: ["es"],
+         cssFileName: "style"
+       }
+     },
+    resolve: {
+     alias: {
+       "@": fileURLToPath(new URL("./src", import.meta.url))
+     }
     }
   }
 });
